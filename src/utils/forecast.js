ffaa3c7fa3b02630517cae277e166ae6d288a791
Priyanka Kumbhar <priya.12891.pk@gmail.com>
2021-03-16 14:15:26 +0530

    changes in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude,longitude, callback) => {
         }else if(response.body.error){
             callback("Unable to find location",undefined)
         }else{
-            callback(undefined,response.body.current.weather_descriptions[0]+' It is currently '+response.body.current.temperature+' degrees out. There is '+response.body.current.precip+'%chance of rain')
+            callback(undefined,response.body.current.weather_descriptions[0]+' It is currently '+response.body.current.temperature+' degrees out. It feels like '+response.body.current.feelslike+' degrees out. the humidity is '+response.body.current.humidity+'%')
         }
     })
 }
